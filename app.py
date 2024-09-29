3a6d05d16afecd7904d74ab3a60205c2542c60f4
Juan Beltrán <beltranrodriguezjuan9@gmail.com>
2024-09-29 08:48:22 -0500

    Vista Proveedor

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template,request
 import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 app = Flask(__name__)
+user = ''
+def guardarNombre(nombre):
+    global user
+    user = nombre
 
 def leerUsuarios():
     df = pd.read_csv('users.csv')
@@ -25,6 +32,20 @@ def guardarUsuario(username, password, account):
     df = leerUsuarios()
     df = pd.concat([df,df_new], ignore_index=True)
     df.to_csv('users.csv',index=False)
+
+def guardarPedido(producto,nombreCliente,nombreProveedor,cantidad,fechaPedida,fechaEntrega,estado):
+    df_new = pd.DataFrame([{
+        "producto":producto,
+        "nombreCliente":nombreCliente,
+        "nombreProveedor":nombreProveedor,
+        "cantidad":cantidad,
+        "fechaPedida":fechaPedida,
+        "fechaEntrega":fechaEntrega,
+        "estado":estado
+    }])
+    df = leerPedidos()
+    df = pd.concat([df,df_new], ignore_index=True)
+    df.to_csv('pedidos.csv',index=False)
 
 @app.route('/')
 def index():  # put application's code here
@@ -57,12 +78,15 @@ def login():  # put application's code here
         usuario_valido = df[(df['username'] == username) & (df['password'] == password)]
 
         if not usuario_valido.empty:
+
+            guardarNombre(usuario_valido['username'].iloc[0])
+
             if usuario_valido['account'].iloc[0] == "Cliente":
                 return render_template('cliente_index.html', producto = leerProductoCliente())
             elif usuario_valido['account'].iloc[0] == "Transportador":
-                return render_template('registro.html')
+                return render_template('proveedor_index.html',producto = leerPedidos().to_dict(orient='records'))
             elif usuario_valido['account'].iloc[0] == "Proveedor":
-                return render_template('registro.html')
+                return render_template('proveedor_index.html',producto = leerPedidos().to_dict(orient='records'))
         else:
             return render_template('index.html')
 
@@ -72,8 +96,36 @@ def login():  # put application's code here
 def indexCliente():  # put application's code here
     return render_template('cliente_index.html', producto = leerProductoCliente())
 
-@app.route('/pedirProducto')
+@app.route('/pedirProducto', methods=['GET', 'POST'])
 def pedirProducto():
+    if request.method == 'POST':
+        producto = request.form['producto']
+        precio = request.form['precio']
+        nombreProveedor = request.form['nombreProveedor']
+        nombreCliente = user
+        fechaPedido = datetime.now()
+        fechaEntrega = fechaPedido + relativedelta(months=1)
+        return render_template('confirmarPedido.html', producto=producto, precio=precio,
+                               nombreProveedor=nombreProveedor, nombreCliente=nombreCliente,
+                               fechaEntrega=fechaEntrega.strftime('%d-%m-%Y'))
+
+    return render_template('pedirProducto.html', producto = leerProductoProveedor().to_dict(orient='records'))
+
+@app.route('/confirmarProducto', methods=['GET', 'POST'])
+def confirmarProducto():
+    if request.method == 'POST':
+        producto = request.form['producto']
+        nombreProveedor = request.form['nombreProveedor']
+        nombreCliente = user
+        cantidad = request.form['cantidad']
+        fechaPedida = datetime.now().strftime('%d-%m-%Y')
+        fechaEntrega = request.form['fechaEntrega']
+        estado = "No Enviado"
+
+        guardarPedido(producto, nombreCliente, nombreProveedor, cantidad, fechaPedida, fechaEntrega, estado)
+
+        return render_template('pedidosCliente.html')
+
     return render_template('pedirProducto.html', producto = leerProductoProveedor().to_dict(orient='records'))
 
 @app.route('/pedidosCliente')
